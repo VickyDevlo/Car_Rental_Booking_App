b5a6a60c01f1455d5b72c51affceda36c904517f
vickyDevlo <vickymagare1996@gmail.com>
2025-07-01 12:18:21 +0530

    setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-2xl text-center font-semibold mt-3">
+        Car Rental Booking App
+      </h1>
+    </div>
+  );
+}
+
+export default App;
