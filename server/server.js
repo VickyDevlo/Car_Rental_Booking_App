9161596a41db7d7278842c21549e9a6252c28d38
vickyDevlo <vickymagare1996@gmail.com>
2025-07-28 17:50:15 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ import { userRouter } from "./routes/userRoutes.js";
 import { ownerRouter } from "./routes/ownerRoutes.js";
 import { bookingRouter } from "./routes/bookingRoutes.js";
 
+const allowedOrigins = [
+  "http://localhost:5173", // Vite local
+  "https://car-rental-booking-app.vercel.app", // Production frontend
+];
 // initialize express app
 const app = express();
 
@@ -13,7 +17,18 @@ const app = express();
 await connectDB();
 
 // middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // basic route
@@ -23,7 +38,6 @@ app.get("/", (_, res) => res.send("Server is running..."));
 app.use("/api/user", userRouter);
 app.use("/api/owner", ownerRouter);
 app.use("/api/bookings", bookingRouter);
-
 
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
