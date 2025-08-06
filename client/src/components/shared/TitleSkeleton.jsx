4494adfef14c55aa755156b2c3284bb03f38570a
vickyDevlo <vickymagare1996@gmail.com>
2025-08-06 12:31:15 +0530

    titleSkeleton updated

--- client/src/components/shared/TitleSkeleton.jsx
+++ client/src/components/shared/TitleSkeleton.jsx
@@ -1,10 +1,9 @@
-
 export const TitleSkeleton = () => {
   return (
-    <div className="flex flex-col gap-1 w-full animate-pulse">
-          <div className="h-7 md:h-10 w-52 md:w-80 bg-gray-200 rounded" />
-          <div className="h-4 max-w-xl bg-gray-200 rounded mt-2" />
-          <div className="h-4 w-56 md:w-96 bg-gray-200 rounded" />
-        </div>
-  )
-}
+    <div className="flex flex-col gap-1 items-center sm:items-start w-full animate-pulse">
+      <div className="h-7 md:h-10 w-45 md:w-80 bg-gray-200 rounded" />
+      <div className="h-4 w-60 md:w-xl bg-gray-200 rounded mt-2" />
+      <div className="h-4 w-40 md:w-96 bg-gray-200 rounded" />
+    </div>
+  );
+};
