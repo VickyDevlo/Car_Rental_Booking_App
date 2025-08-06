af4eb56b37b653b8aada8dfcf0d99fa4345d2d9e
vickyDevlo <vickymagare1996@gmail.com>
2025-08-06 12:08:15 +0530

    animated done

--- client/src/pages/changePassword/ChangePassword.jsx
+++ client/src/pages/changePassword/ChangePassword.jsx
@@ -197,9 +197,9 @@ const ChangePassword = () => {
             {isMatch && (
               <AnimatePresence>
                 <motion.div
-                  initial={{ opacity: 0, x: 20 }}
+                  initial={{ opacity: 0, x: 10 }}
                   animate={{ opacity: 1, x: 0 }}
-                  exit={{ opacity: 0, x: 20 }}
+                  exit={{ opacity: 0, x: 10 }}
                   transition={{ duration: 0.3 }}
                   className="ml-2"
                 >
